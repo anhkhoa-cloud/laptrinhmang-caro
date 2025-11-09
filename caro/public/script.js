4f99622909cd4b910e4be1b3040d91f2794cd50b
hungtran-icloud <hungtq4106@ut.edu.vn>
2025-11-09 21:53:01 +0700

    Add CaroGame class with initialization logic

--- caro/public/script.js
+++ caro/public/script.js
@@ -1,1 +1,40 @@
+class CaroGame {
+    constructor() {
+        this.socket = io();
+        this.playerId = null;
+        this.playerSymbol = null;
+        this.gameState = null;
+        this.isMyTurn = false;
+        this.roomId = null;
+        
+        this.initializeElements();
+        this.setupEventListeners();
+        this.setupSocketListeners();
+        this.createBoard();
+        this.toggleRoomActions(false);
+        this.setChatEnabled(false);
+        this.updateRoomInfoUI();
+        this.setLeaveRoomEnabled(false);
+    }
 
+    initializeElements() {
+        this.gameBoard = document.getElementById('gameBoard');
+        this.gameStatus = document.getElementById('gameStatus');
+        this.currentPlayer = document.getElementById('currentPlayer');
+        this.player1 = document.getElementById('player1');
+        this.player2 = document.getElementById('player2');
+        this.roomIdDisplay = document.getElementById('roomIdDisplay');
+        this.copyRoomIdBtn = document.getElementById('copyRoomIdBtn');
+
+        this.createRoomBtn = document.getElementById('createRoomBtn');
+        this.joinRoomBtn = document.getElementById('joinRoomBtn');
+        this.resetBtn = document.getElementById('resetBtn');
+        this.leaveRoomBtn = document.getElementById('leaveRoomBtn');
+
+        this.createRoomModal = document.getElementById('createRoomModal');
+        this.joinRoomModal = document.getElementById('joinRoomModal');
+        this.gameOverModal = document.getElementById('gameOverModal');
+
+        
+    }
+  
